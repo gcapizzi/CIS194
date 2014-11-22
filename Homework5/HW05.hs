df31120f34471bad7014cc45276f431337221863
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-11-23 00:32:04 +0100

    Start solving HW05

--- Homework5/HW05.hs
+++ Homework5/HW05.hs
@@ -2,3 +2,37 @@ module HW05 where
 
 import Ring
 import Parser
+
+data Mod5 = Mod5 Integer deriving (Show, Eq)
+
+instance Ring Mod5 where
+  addId = Mod5 0
+  addInv (Mod5 x) = Mod5 (negate x)
+  add (Mod5 x) (Mod5 y) = Mod5 ((x + y) `mod` 5)
+  mulId = Mod5 1
+  mul (Mod5 x) (Mod5 y) = Mod5 ((x * y) `mod` 5)
+
+instance Parsable Mod5 where
+  parse x
+    | Just (n, r) <- (parse x) = Just ((Mod5 n), r)
+    | otherwise = Nothing
+
+data Mat2x2 = Mat2x2 Integer Integer Integer Integer deriving (Show, Eq)
+
+instance Ring Mat2x2 where
+  add (Mat2x2 l11 l12 l21 l22) (Mat2x2 r11 r12 r21 r22) = (Mat2x2 n11 n12 n21 n22)
+    where n11 = l11 + r11
+          n12 = l12 + r12
+          n21 = l21 + r21
+          n22 = l22 + r22
+  addId = (Mat2x2 0 0 0 0)
+  addInv (Mat2x2 a b c d) = (Mat2x2 (negate a) (negate b) (negate c) (negate d))
+  mul (Mat2x2 l11 l12 l21 l22) (Mat2x2 r11 r12 r21 r22) = (Mat2x2 n11 n12 n21 n22)
+    where n11 = (l11 * r11) + (l12 * r21)
+          n12 = (l11 * r12) + (l12 * r22)
+          n21 = (l21 * r11) + (l22 * r21)
+          n22 = (l21 * r12) + (l22 * r22)
+  mulId = (Mat2x2 1 0 0 1)
+
+instance Parsable Mat2x2 where
+  parse x = Just ((Mat2x2 0 0 0 0), "")
---
+++ Homework5/HW05Spec.hs
@@ -0,0 +1,38 @@
+import Test.Hspec
+
+import Ring
+import Parser
+
+import HW05
+
+main :: IO ()
+main = hspec $ do
+  describe "Mod5" $ do
+    it "supports addition" $ do
+      add (Mod5 1) (Mod5 2) `shouldBe` (Mod5 3)
+      add (Mod5 3) (Mod5 2) `shouldBe` (Mod5 0)
+    it "uses 0 as the additive identity" $ do
+      add (Mod5 3) addId `shouldBe` (Mod5 3)
+    it "uses negation as the additive inverse" $ do
+      add (Mod5 3) (addInv (Mod5 3)) `shouldBe` addId
+    it "supports multiplication" $ do
+      mul (Mod5 2) (Mod5 2) `shouldBe` (Mod5 4)
+      mul (Mod5 2) (Mod5 3) `shouldBe` (Mod5 1)
+    it "uses 1 as the multiplicative identity" $ do
+      mul (Mod5 2) mulId `shouldBe` (Mod5 2)
+    it "is parseable" $ do
+      parseRing "1 + 2 * 5" `shouldBe` Just (Mod5 1)
+
+  describe "Mat2x2" $ do
+    it "supports addition" $ do
+      add (Mat2x2 1 2 3 4) (Mat2x2 5 6 7 8) `shouldBe` (Mat2x2 6 8 10 12)
+    it "uses [[0,0][0,0]] as the additive identity" $ do
+      add (Mat2x2 1 2 3 4) addId `shouldBe` (Mat2x2 1 2 3 4)
+    it "uses negation as the additive inverse" $ do
+      add (Mat2x2 1 2 3 4) (addInv (Mat2x2 1 2 3 4)) `shouldBe` addId
+    it "supports multiplication" $ do
+      mul (Mat2x2 1 2 3 4) (Mat2x2 5 6 7 8) `shouldBe` (Mat2x2 19 22 43 50)
+    it "uses [[1,0][0,1]] as the multiplicative identity" $ do
+      mul (Mat2x2 1 2 3 4) mulId `shouldBe` (Mat2x2 1 2 3 4)
+    it "is parseable" $ do
+      parseRing "[[1,2][3,4]] + [[5,6][7,8]]" `shouldBe` Just (Mat2x2 19 22 43 50)

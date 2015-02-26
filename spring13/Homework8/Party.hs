4424fc0b7dcd0939de942b674329ac04ea8c1f97
Giuseppe Capizzi <g.capizzi@gmail.com>
2015-02-26 22:07:44 +0100

    spring13/Homework 8 - Ex 2

--- spring13/Homework8/Party.hs
+++ spring13/Homework8/Party.hs
@@ -2,6 +2,8 @@ module Party where
 
 import Employee
 import Data.Monoid
+import Data.Tree
+import Debug.Trace
 
 glCons :: Employee -> GuestList -> GuestList
 glCons employee (GL employees fun) = GL (employee:employees) (fun + empFun employee)
@@ -11,5 +13,10 @@ instance Monoid GuestList where
   mappend (GL xs xfun) (GL ys yfun) = GL (xs ++ ys) (xfun + yfun)
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun xs@(GL _ xfun) ys@(GL _ yfun) | xfun >= yfun = xs
-moreFun _ ys = ys
+moreFun xs@(GL _ xfun) ys@(GL _ yfun)
+  | xfun >= yfun = xs
+  | otherwise    = ys
+
+treeFold :: (Show a, Show b) => ([b] -> a -> b) -> b -> Tree a -> b
+treeFold f z (Node x []) = f [z] x
+treeFold f z (Node x subTrees) = f (map (treeFold f z) subTrees) x
